fc9de2b89e41700c3aa5db5a93db2b8c3a9257f5
gustavobiel <56567873+gustavobiel@users.noreply.github.com>
2020-11-05 00:11:06 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/ucsist/projetomap/ui/CadastroActivity.java
@@ -0,0 +1,77 @@
+package br.usjt.ucsist.projetomap.ui;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.orhanobut.hawk.Hawk;
+
+import br.usjt.ucsist.projetomap.R;
+import br.usjt.ucsist.projetomap.model.Usuario;
+import br.usjt.ucsist.projetomap.model.UsuarioViewModel;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    private UsuarioViewModel usuarioViewModel;
+    private Usuario usuarioCorrente;
+    private EditText editTextNome;
+    private EditText editTextCPF;
+    private EditText editTextEmail;
+    private EditText editTextSenha;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+        Hawk.init(this).build();
+
+        editTextNome = findViewById(R.id.editTextNome);
+        editTextCPF = findViewById(R.id.editTextCpf);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextSenha = findViewById(R.id.editTextSenha);
+
+        usuarioViewModel = new ViewModelProvider(this).get(UsuarioViewModel.class);
+
+        usuarioViewModel.getUsuario().observe(this, new Observer<Usuario>() {
+            @Override
+            public void onChanged(@Nullable final Usuario usuario) {
+                updateView(usuario);
+            }
+        });
+    }
+
+    private void updateView(Usuario usuario){
+        if(usuario != null && usuario.getId() > 0){
+            usuarioCorrente = usuario;
+            editTextNome.setText(usuario.getNome());
+            editTextCPF.setText(usuario.getCpf());
+            editTextEmail.setText(usuario.getEmail());
+            editTextSenha.setText(usuario.getSenha());
+        }
+    }
+
+
+    public void salvar(View view) {
+
+        if(usuarioCorrente == null){
+            usuarioCorrente = new Usuario();
+        }
+        usuarioCorrente.setNome(editTextNome.getText().toString());
+        usuarioCorrente.setCpf(editTextCPF.getText().toString());
+        usuarioCorrente.setEmail(editTextEmail.getText().toString());
+        usuarioCorrente.setSenha(editTextSenha.getText().toString());
+        usuarioViewModel.insert(usuarioCorrente);
+        Toast.makeText(this,"Usuário salvo com sucesso",
+                Toast.LENGTH_SHORT).show();
+        Hawk.put("tem_cadastro",true);
+        finish();
+    }
+}
